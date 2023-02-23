7af6958c94c1e33af6d2b77a833504b28b7d9222
Rolf Strijdhorst <rolfst@gmail.com>
2023-02-23 09:36:43 +0100

    chore: disable secrets for now

--- configuration/secrets/default.nix
+++ configuration/secrets/default.nix
@@ -1,21 +1,21 @@
-{ config, secrets, ... }:
+{ config, ... }: # TODO add secrets when I've figured those out
 
 let
   # secretsPath = /secrets/nixos-configuration/secrets;
   user = config.users.users.rolfst.name;
 in {
-  sops.validateSopsFiles = false;
+  # sops.validateSopsFiles = false;
 
-  sops.defaultSopsFile = secrets + "/secrets.yaml";
+  # sops.defaultSopsFile = secrets + "/secrets.yaml";
 
   # Allow user to read keys
   users.users.rolfst.extraGroups = [ config.users.groups.keys.name ];
 
-  sops.secrets.freedns = { };
-  sops.secrets.desktop-ssh-key = {
-    format = "binary";
-    # sopsFile = secretsPath + "/secret-desktop-ssh-key";
-    sopsFile = secrets + "/secrets/secret-desktop-ssh-key";
-    owner = user;
-  };
+  # sops.secrets.freedns = { };
+  # sops.secrets.desktop-ssh-key = {
+  #   format = "binary";
+  #   # sopsFile = secretsPath + "/secret-desktop-ssh-key";
+  #   sopsFile = secrets + "/secrets/secret-desktop-ssh-key";
+  #   owner = user;
+  # };
 }
